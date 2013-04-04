13d64eb9ddf6f9c8c44ac9d5674b157ab63f2de8
Robert <robert@privatdemail.net>
2013-04-04 10:51:55 +0200

    support fahrenheit in cputemp plugin

--- plugins/cputemp.lua
+++ plugins/cputemp.lua
@@ -5,7 +5,7 @@ PLUGIN_infoCollector0_important = true
 data = ""
 
 function getContent()
-	return data .. "°C"
+	return data
 end
 function infoCollector0()
 	-- open temperature file:
@@ -17,7 +17,14 @@ function infoCollector0()
 	-- read:
 	line = f.read(f)
 	-- calculate temperature:
-	data = line/1000
+	t_c = line/1000
+	t_f = (t_c*1.8)+32
+	-- set data:
+	if PLUGINCONF_fahrenheit and PLUGINCONF_fahrenheit == "1" then
+		data = t_f .. "°F"
+	else
+		data = t_c .. "°C"
+	end
 	-- close file:
 	f.close(f)
 	-- return success:
